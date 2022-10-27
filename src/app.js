bb7a1c3b0c3141a02bd354c39dea68a8c090315b
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-10-27 20:49:20 +0100

    Accept JSON data into API

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({extended: false}));
 connection();
 
 app.get("/", (_, res) => {
